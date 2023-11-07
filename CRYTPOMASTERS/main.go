d1b7fa4a33e4481c623a397b15d2704fcb5d06df
mayura-andrew <mayuraalahakoon@gmail.com>
2023-11-07 14:49:31 +0530

    Mapping Between JSOn and Go Struct

--- CRYTPOMASTERS/main.go
+++ CRYTPOMASTERS/main.go
@@ -8,7 +8,16 @@ import (
 
 
 func main(){
-	rate, err := api.GetRate("BTC")
+	//rate, err := api.GetRate("BTC")
+	getCurrencyData("BTC")
+	getCurrencyData("ETH")
+	getCurrencyData("BCH")
+
+
+}
+
+func getCurrencyData(currency string){
+	rate, err := api.GetRate(currency)
 	if err == nil {
 		fmt.Printf("The rate for %v is %.2f \n", rate.Currency, rate.Price)
 	}
